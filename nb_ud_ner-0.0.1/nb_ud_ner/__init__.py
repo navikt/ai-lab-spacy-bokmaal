1c19933db95a00a6bbff31ca01e8e1c209570df2
Kaisa Korsak <K153870@kaisas-mbp.adeo.no>
2018-08-03 14:11:22 +0200

    Adding more data and more string preprocessing + forcing entities to have PROPN tag

--- nb_ud_ner-0.0.1/nb_ud_ner/__init__.py
+++ nb_ud_ner-0.0.1/nb_ud_ner/__init__.py
@@ -45,6 +45,13 @@ class EntityMatcher(object):
             entity = Span(doc, start, end, label=label_id)
             entity._.via_patterns = True
             spans.append(entity)
+            
+            for token in doc:
+                #can't force pos_, because pos_ is decided from tag_
+                #force tag_ to be PROPN, all named entities are PROPN
+                #otherwise 'Hvaler' will be NOUN
+                if token.text == entity.text:
+                    token.tag_ = 'PROPN___'
         doc.ents = list(doc.ents) + spans  # overwrite doc.ents and add entities – don't replace!
         for span in spans:
             span.merge()  # merge all spans at the end to avoid mismatched indices
@@ -56,7 +63,6 @@ class EntityMatcher(object):
         script_dir = os.path.dirname(__file__)
         #find all .csv files in this directory's entity_matcher folder
         path = glob.glob(script_dir + '/entity_matcher/*csv')
-        print(label)
         for filename in path:
             filename2 = re.match('.*?entity_matcher/(\w+).csv', filename)
             if filename2 != None:
@@ -65,7 +71,7 @@ class EntityMatcher(object):
                 if label.lower() in file_label:
                     first_line_skipped = False
                     #column in csv file to read from
-                    column = 0
+                    columns = []
                     encoding = 'utf-8'
                     if label == 'LOC':
                         encoding = 'latin-1'
@@ -74,33 +80,56 @@ class EntityMatcher(object):
                             line = line.split(";")
                             #skip the first line containg heading etc
                             if not first_line_skipped:
-                                column = self.find_column_in_file(column, line)
+                                columns = self.find_column_in_file(columns, line)
                                 first_line_skipped = True
                                 continue
-                            entity = self.do_string_preprocessing(line[column], label)
-                            print(entity)
-                            words = entity.split()
-                            #PhraseMatcher only supports phrases up to 10 tokens
-                            if len(words) < 10:
-                                entity_list.append(entity.title())
+                            for column in columns:
+                                entity = self.do_string_preprocessing(line[column], label)
+                                entity_list = self.add_new_entities_to_list(entity, file_label, entity_list)
+
         return entity_list
 
-    def find_column_in_file(self, column, line):
+    def add_new_entities_to_list(self, entity, file_label, entity_list):
+        words = entity.split()
+        #PhraseMatcher only supports phrases up to 10 tokens
+        #don't add duplicates
+        #don't allow empty tokens, it throws ValueError: [T001] Max length currently 10 for phrase matching
+        if len(words) < 10 and entity.title() not in entity_list and entity != "":
+            #the cities' names with / in this file have to be split in 2 separate entities
+            if file_label == 'tettsteder_loc' and '/' in entity:
+                entities = entity.split('/')
+                for e in entities:
+                    entity_list.append(e.title())
+            else:
+                entity_list.append(entity.title())
+        return entity_list
+
+    def find_column_in_file(self, columns, line):
         cat_counter = 0
-        #find out which column contains names
+        #find out which columns contain names
         for cat in line:
-            if "navn" in cat or "name" in cat:
-                column = cat_counter
-                break
+            cat = cat.lower().strip("\"")
+            if cat == 'navn' or cat == 'name' or cat == 'shortname' or cat == 'tettsted' or cat == 'kommune':
+                columns.append(cat_counter)
             cat_counter += 1
-        return column
+        return columns
 
     def do_string_preprocessing(self, name, label):
-        #remove punctuation etc
-        entity = re.sub(r'[^\w\s]','',name)
-        #only remove digits from names, we want to keep them for organizations etc 
-        if label == 'PER':
-            entity = re.sub(r'[\d\s]','',entity)
+        #only remove digits from names and locations, we want to keep them for organizations etc 
+        #keep - / and . in locations and names (U.S.A., Bosnia-Hercegovina, Trinid/Tobago)
+        if label != 'ORG' or label != 'MISC':
+            entity = re.sub(r'[^\w\s/-/.]','',name)
+            #entity = re.sub(r'[\d\s]','',entity)
+            entity = re.sub(r'[\d]','',entity)
+            #only remove whitespace at the beginning
+            entity = re.sub(r'^[\s]','',entity)
+            entity = re.sub(r'$[\s]','',entity)
+        else:
+            #remove punctuation etc
+            entity = re.sub(r'[^\w\s]','',name)
+        if label == 'LOC':
+            #print(entity)
+            entity = re.sub(r'i alt','',entity)
         #remove extra whitespace after removing punctuation, otherwise it will end up as a token
         entity = re.sub(' +',' ',entity)
         return entity
