4da2652502551b166e68017ee415b7da2f736226
Kaisa Korsak <K153870@kaisas-mbp.adeo.no>
2018-08-03 10:08:29 +0200

    All the files are now read from entity_matcher in the same directory as __init__.py, added most popular surnames and girls' and boys' names

--- nb_ud_ner-0.0.1/nb_ud_ner/__init__.py
+++ nb_ud_ner-0.0.1/nb_ud_ner/__init__.py
@@ -25,15 +25,15 @@ class EntityMatcher(object):
     name = 'entity_matcher'  # component name shown in pipeline
 
     def __init__(self, nlp):
-        #self.name = name # component name shown in pipeline
         patterns = defaultdict(list)
         Span.set_extension('via_patterns', default=False)
-        labels = ['ORG', 'LOC', 'PER', 'MISC']
+        labels = ['LOC', 'PER', 'ORG', 'MISC']
         self.matcher = PhraseMatcher(nlp.vocab)
         #get entities with given label and add to matcher for each label in list
         for label in labels:
             self.entities = self.get_entities(label)
             patterns[label] = [nlp(text) for text in self.entities]
+
         for label, pattern in patterns.items():
             self.matcher.add(label, None,  *pattern)
         
@@ -52,22 +52,55 @@ class EntityMatcher(object):
 
     def get_entities(self, label):
         entity_list = []
-        #get entities tagged with that label from a file label.csv and return a list of those entities
-        filename = label.lower() + ".csv"
-        path = 'entity_matcher/'
-        filename = os.path.join(path, filename)
-        first_line_skipped = False
-        with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                #skip the first line containg heading etc
-                if not first_line_skipped:
-                    first_line_skipped = True
-                    continue
-                line = line.split(";")
-                entity = re.sub(r'[^\w\s]','',line[1])
-                #remove extra whitespace after removing punctuation, otherwise it will end up as a token
-                entity = re.sub(' +',' ',entity)
-                words = entity.split()
-                if len(words) < 10:
-                    entity_list.append(entity.title())
+        #get directory of this script
+        script_dir = os.path.dirname(__file__)
+        #find all .csv files in this directory's entity_matcher folder
+        path = glob.glob(script_dir + '/entity_matcher/*csv')
+        print(label)
+        for filename in path:
+            filename2 = re.match('.*?entity_matcher/(\w+).csv', filename)
+            if filename2 != None:
+                file_label = filename2.group(1)
+                #find out which files contain entities with the label we are looking for
+                if label.lower() in file_label:
+                    first_line_skipped = False
+                    #column in csv file to read from
+                    column = 0
+                    encoding = 'utf-8'
+                    if label == 'LOC':
+                        encoding = 'latin-1'
+                    with open(filename, 'r', encoding=encoding) as file:
+                        for line in file:
+                            line = line.split(";")
+                            #skip the first line containg heading etc
+                            if not first_line_skipped:
+                                column = self.find_column_in_file(column, line)
+                                first_line_skipped = True
+                                continue
+                            entity = self.do_string_preprocessing(line[column], label)
+                            print(entity)
+                            words = entity.split()
+                            #PhraseMatcher only supports phrases up to 10 tokens
+                            if len(words) < 10:
+                                entity_list.append(entity.title())
         return entity_list
+
+    def find_column_in_file(self, column, line):
+        cat_counter = 0
+        #find out which column contains names
+        for cat in line:
+            if "navn" in cat or "name" in cat:
+                column = cat_counter
+                break
+            cat_counter += 1
+        return column
+
+    def do_string_preprocessing(self, name, label):
+        #remove punctuation etc
+        entity = re.sub(r'[^\w\s]','',name)
+        #only remove digits from names, we want to keep them for organizations etc 
+        if label == 'PER':
+            entity = re.sub(r'[\d\s]','',entity)
+        #remove extra whitespace after removing punctuation, otherwise it will end up as a token
+        entity = re.sub(' +',' ',entity)
+        return entity
